419ef327b7bce1d7f8408318fce8fbc6a5bba207
jhonny leandro melo escobar <ejohhnny@gmail.com>
2022-08-08 20:50:47 -0500

    Adding test for checkboxes and deleting test1 spec

--- cypress/integration/examples/controls.spec.js
+++ cypress/integration/examples/controls.spec.js
@@ -1,6 +1,29 @@
-describe('My First Cypress Test', function() {
-    it('Visits the ToolsQA Demo Page and check the menu items', function() {
-        //Visit the Demo QA Website
-        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+describe('Testing web controls', function() {
+
+    beforeEach(()=>{
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
     })
+
+    it('testing checkboxes', function() {
+        cy.get('#checkBoxOption1')
+          .as('checkbox1')
+
+        cy.get('@checkbox1')
+          .check()
+          .should('be.checked')
+          .and('have.value', 'option1')
+
+        cy.get('@checkbox1')
+          .uncheck()
+          .should('not.be.checked')
+
+        cy.get('input[type="checkbox"]')
+          .check(['option2', 'option3'])
+    })
+
+    
+    it('testing selects', function() {
+
+    })
+
 })
--- cypress/integration/examples/test1.spec.js
+++
@@ -1,9 +0,0 @@
-// type definitions for Cypress object "cy"
-/// <reference types="cypress" />
-
-describe('My First Cypress Test', function() {
-    it('Visits the ToolsQA Demo Page and check the menu items', function() {
-    //Visit the Demo QA Website
-    cy.visit("https://demoqa.com/");
-})
-})
