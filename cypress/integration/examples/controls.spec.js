e15d5dea56e47de028f4cda12078dfefc735c469
jhonny leandro melo escobar <ejohhnny@gmail.com>
2022-08-08 20:25:01 -0500

    Add cypress configuration files

---
+++ cypress/integration/examples/controls.spec.js
@@ -0,0 +1,6 @@
+describe('My First Cypress Test', function() {
+    it('Visits the ToolsQA Demo Page and check the menu items', function() {
+        //Visit the Demo QA Website
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    })
+})
